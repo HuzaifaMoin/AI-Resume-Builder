7b04dfda75ac356d2c087b2ee403b084bf7cbe33
Huzaifa Moin <huzaifamoin784@gmail.com>
2026-07-21 20:11:15 +0500

    complete edit and delete UI for Dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Layout from './pages/Layout'
 import Dashboard from './pages/Dashboard'
+import ResumeBuilder from './pages/ResumeBuilder'
 import Preview from './pages/Preview'
 import Login from './pages/Login'
 
@@ -13,7 +14,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='app' element={<Layout />} > 
           <Route index element={<Dashboard />}/>
-          <Route path='builder/:resumeId' element={<Dashboard />}/>
+          <Route path='builder/:resumeId' element={<ResumeBuilder />}/>
         </Route>
         <Route path='view/:resumeId' element={<Preview />}/>
         <Route path='login' element={<Login />}/>
